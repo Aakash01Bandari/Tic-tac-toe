442739b136fc4d920849db9cd92ca271615c2c75
Aakash Bandari <akash.ban@yahoo.com>
2019-05-18 16:20:53 -0400

    Added winner feature

--- src/Game.js
+++ src/Game.js
@@ -16,6 +16,34 @@ const getPoints = (move) => {
 
     return points[move];
 };
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i += 1) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return {
+                winner: squares[a],
+                winnerRow: lines[i],
+            };
+        }
+    }
+
+    return {
+        winner: null,
+        winnerRow: null,
+    };
+};
+
 
 class Game extends React.Component {
     constructor(props) {
@@ -66,15 +94,20 @@ class Game extends React.Component {
     render() {
         const { history } = this.state;
         const current = history[this.state.currentStepNumber];
+        const { winner, winnerRow } = calculateWinner(current.squares);
 
         let status;
-
+        if (winner) {
+            status = `Winner ${winner}`;
+        } else {
             status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
         return (
             <div className="game">
                 <div className="game-board">
                     <Board
                         squares={current.squares}
+                        winnerSquares={winnerRow}
                         onClick={i => this.handleClick(i)}
                     />
                 </div>
