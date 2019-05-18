9df0b907af1d4cf526bf63a8ef86c14840906cf8
Aakash Bandari <akash.ban@yahoo.com>
2019-05-18 01:09:15 -0400

    Minor changes

--- src/Game.js
+++ src/Game.js
@@ -57,8 +57,7 @@ class Game extends React.Component {
 
         return (
             <div className="game">
-                <div>
-                    Tic tac toe
+                <div className="game-board">
                     <Board
                         squares={current.squares}
                         onClick={i => this.handleClick(i)}
