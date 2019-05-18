cf4eafa8ac05038fbb9c8dc5c70d7ed252d89f99
Aakash Bandari <akash.ban@yahoo.com>
2019-05-18 16:51:31 -0400

    Added game draw code

--- src/Game.js
+++ src/Game.js
@@ -99,6 +99,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = `Winner ${winner}`;
+        } else if (history.length === 10) {
+            status = 'Game Draw';
         } else {
             status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
         }
