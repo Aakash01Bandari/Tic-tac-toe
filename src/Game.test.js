ebfa07024903bd5b9443b13616ed2ab632d13241
Aakash Bandari <akash.ban@yahoo.com>
2019-05-20 22:56:42 -0400

    Test case 1

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
-import { configure } from 'enzyme';
+import { configure,shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
@@ -9,4 +9,9 @@ it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Game />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Game component', () => {
+    const component = shallow(<Game />);
+    expect(component).toHaveLength(1);
 });
