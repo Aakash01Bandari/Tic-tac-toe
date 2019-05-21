61e8ec3a9510f33a23cc49ab0c43e1a323d8c7ce
Aakash Bandari <akash.ban@yahoo.com>
2019-05-20 21:45:40 -0400

    Configuring enzyme

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<Game />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
