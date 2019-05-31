3fca7657d7917a8f326ffdbfb2405a054966fb9b
Aakash01Bandari <ayushi.m@consultadd.in>
2019-05-31 11:09:11 +0530

    Updates

---
+++ precache-manifest.524906625f5937bceb0b41b6d19f47dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd561bf1dd61bee2bdb10208391abe32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e796ff8be6db3372c38",
+    "url": "/static/css/main.cd0c321d.chunk.css"
+  },
+  {
+    "revision": "20d1bb46ff6c583b9b3b",
+    "url": "/static/js/2.5fd1c813.chunk.js"
+  },
+  {
+    "revision": "9e796ff8be6db3372c38",
+    "url": "/static/js/main.5f631bf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
